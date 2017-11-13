2af3703839dfea12cae0d302a4c23514c3d920fa
HJS <407410014@qq.com>
2017-11-13 16:54:10 +0800

    little update

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,17 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 
 public class WordNet {
+    private static final String COMMA = ",";
     
+    private static final String SPACE = " ";
+    
+    private TreeMap<Integer, String[]> synsetss;
+    
+    private Digraph graphs;
     
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
     {
         validate(synsets);
         validate(hypernyms);
+        synsetss = new TreeMap<>();
+        
+        In in = new In(synsets);
+        while(in.hasNextLine()) {
+            String[] s = in.readLine().split(COMMA);
+            
+            int id = Integer.parseInt(s[0]);
+            String[] nouns = s[1].split(SPACE);
+            
+            synsetss.put(id, nouns);
+        }
+        
+        in = new In(hypernyms);
+        graphs = new Digraph(synsetss.size());
+        
+        while(in.hasNextLine()) {
+            String[] s = in.readLine().split(COMMA);
+            
+            int son = Integer.parseInt(s[0]);
+            String[] fathers = Arrays.copyOfRange(s, 1, s.length);
+            
+            for (String t : fathers)
+                graphs.addEdge(son, Integer.parseInt(t));
+        }
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() 
     {
+        Collection<String[]> strs = synsetss.values();
         return null;
     }
 
@@ -41,6 +80,15 @@ public class WordNet {
     private void validate(String s) 
     {
         if (s == null) throw new java.lang.IllegalArgumentException();
+    }
+    
+    private static class Synset {
+        int id;
+        String[] nouns;
+        public Synset(int mId, String... mNouns) {
+            this.id =- mId;
+            this.nouns = mNouns;
+        }
     }
     // do unit testing of this class
     public static void main(String[] args) 
