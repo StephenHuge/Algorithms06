5902e07c1f912e8be996206eb86eb8ae6bbd98e0
HJS <407410014@qq.com>
2017-11-15 19:40:28 +0800

    change name for DeluxBFS.java

--- src/DeluxBFS.java
+++ src/DeluxBFS.java
@@ -11,7 +11,7 @@ import edu.princeton.cs.algs4.StdOut;
  *  
  *  http://blog.csdn.net/liuweiran900217/article/details/22603325
  */
-public class DeluxDFS {
+public class DeluxBFS {
     private static final int INFINITY = Integer.MAX_VALUE;
     private boolean[] marked;  // marked[v] = is there an s->v path?
     private int[] edgeTo;      // edgeTo[v] = last edge on shortest s->v path
@@ -23,7 +23,7 @@ public class DeluxDFS {
      * @param s the source vertex
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public DeluxDFS(Digraph G, int s) {
+    public DeluxBFS(Digraph G, int s) {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
@@ -41,7 +41,7 @@ public class DeluxDFS {
      * @throws IllegalArgumentException unless each vertex {@code v} in
      *         {@code sources} satisfies {@code 0 <= v < V}
      */
-    public DeluxDFS(Digraph G, Iterable<Integer> sources) {
+    public DeluxBFS(Digraph G, Iterable<Integer> sources) {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
