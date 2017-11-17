fb1ef035d870d6a70cbb65a988c5908bae9d11c5
coder-god <407410014@qq.com>
2017-11-18 00:12:52 +0800

    add interface MySCC

---
+++ src/my/graph/hello/MySCC.java
@@ -0,0 +1,21 @@
+package my.graph.hello;
+
+/**
+ * strong connected components
+ */
+public class MySCC {
+    
+    public MySCC(MyDigraph g) {}
+    
+    public boolean stronglyConnected(int v, int w) {
+        return false;
+    }
+    
+    public int count() {
+        return -1;
+    }
+    
+    public int id(int v) {
+        return -1;
+    }
+}
