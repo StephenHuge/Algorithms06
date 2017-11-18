45d81d0a143aeb97e3f22a2f9cacd1f67a18e980
HJS <407410014@qq.com>
2017-11-18 16:13:58 +0800

    add interface MyMST.java

---
+++ src/my/graph/mst/MyMST.java
@@ -0,0 +1,13 @@
+package my.graph.mst;
+
+public class MyMST {
+    public MyMST(EdgeWeightedGraph g) {}
+    
+    public Iterable<Integer> edges() {
+        return null;
+    }
+    
+    public double weight() {
+        return -1.0;
+    }
+}
