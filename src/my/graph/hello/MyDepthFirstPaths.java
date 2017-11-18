7dbd898fabebe5987eaba213bdea9497d41c9623
HJS <407410014@qq.com>
2017-11-18 09:28:22 +0800

    update -- no need to init edgeTo[]

--- src/my/graph/hello/MyDepthFirstPaths.java
+++ src/my/graph/hello/MyDepthFirstPaths.java
@@ -21,9 +21,6 @@ public class MyDepthFirstPaths {
         marked = new boolean[g.V()];
         edgeTo = new int[g.V()];
         this.s = v;
-        for (int i = 0; i < edgeTo.length; i++) {
-            edgeTo[i] = i;
-        }
         dfs(g, v);
     }
     private void dfs(MyDigraph g, int v) {
